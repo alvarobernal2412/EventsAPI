7948217d8fe2597ddf083b4d500b898222b3800a
Alvaro Gonzalez <alvgonfri@alum.us.es>
2022-05-08 20:22:31 +0200

    services.py improve
    Now you can get the weather for a given city, date and hour

--- backend/main/services.py
+++ backend/main/services.py
@@ -1,8 +1,8 @@
 # File for consuming AccuWeather API
 import requests
-import json
+from datetime import datetime
 
-key = 'm4IWCaABNGfRD0fGdJPCcshMKTrlomQV'
+key = 'a0jJ7zlYgnA6EZhtn9G3avV5nAWz1Ofq'
 
 def generate_request(url, params={}):
     response = requests.get(url, params=params)
@@ -15,19 +15,30 @@ def get_city_id(city_name, params={}):
                                 + key + '&q='+ city_name, params)
     if response:
        city = response[0]
-       aux = json.dumps(city) # Convierte a str
-       city_json = json.loads(aux) # Convierte a dict
-       return city_json['Key']
+       return city['Key']
 
-def get_weather_1day(city, params={}):
-    response = generate_request('http://dataservice.accuweather.com/forecasts/v1/daily/1day/' 
-                                + str(get_city_id(city)) + '?apikey=' + key, params)
+def get_weather(city, date, time, params={}):
+    response = generate_request('http://dataservice.accuweather.com/forecasts/v1/daily/5day/' 
+                                + get_city_id(city) + '?apikey=' + key, params)
+
+    date = datetime.strptime(date, "%Y-%m-%d")
+    today = datetime.combine(datetime.today(), datetime.min.time())
+    days_diff = (date-today).days
+    
+    time = datetime.strptime(time, "%X").time()
+    night_start = datetime.strptime("21:00:00", "%X").time()
+    night_end = datetime.strptime("6:00:00", "%X").time()
+
     if response:
-        weather_1day = response
-        aux = json.dumps(weather_1day)
-        weather_1day_json =  json.loads(aux)
-        print(weather_1day['DailyForecasts'])
-        return weather_1day
+        weather = response
+        if days_diff <= 4:
+            if night_start <= time or time <= night_end:
+                print(weather['DailyForecasts'][days_diff]['Night']['IconPhrase'])
+                return weather['DailyForecasts'][days_diff]['Night']['IconPhrase']
+            else:
+                print(weather['DailyForecasts'][days_diff]['Day']['IconPhrase'])
+                return weather['DailyForecasts'][days_diff]['Day']['IconPhrase']
 
 if __name__ == "__main__":
-    get_weather_1day('Seville')
+    get_weather('Seville', "2022-05-11", "21:00:00")
+    
