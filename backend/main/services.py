55458bd4e91a55d8c43c92f47d7e58bf4ab2bab4
Alvaro Gonzalez <alvgonfri@alum.us.es>
2022-05-08 12:59:26 +0200

    Update services.py
    Update services.py in order to get the weather of a city

--- backend/main/services.py
+++ backend/main/services.py
@@ -1,7 +1,8 @@
 # File for consuming AccuWeather API
 import requests
+import json
 
-key = 'a0jJ7zlYgnA6EZhtn9G3avV5nAWz1Ofq'
+key = 'm4IWCaABNGfRD0fGdJPCcshMKTrlomQV'
 
 def generate_request(url, params={}):
     response = requests.get(url, params=params)
@@ -9,22 +10,24 @@ def generate_request(url, params={}):
     if response.status_code == 200:
         return response.json()
 
-def get_city(q, params={}):
+def get_city_id(city_name, params={}):
     response = generate_request('http://dataservice.accuweather.com/locations/v1/cities/search?apikey='
-                                + key + '&q='+ q, params)
+                                + key + '&q='+ city_name, params)
     if response:
        city = response[0]
-       print(city)
-       return city
+       aux = json.dumps(city) # Convierte a str
+       city_json = json.loads(aux) # Convierte a dict
+       return city_json['Key']
 
-def get_weather_1day(city_id, params={}):
+def get_weather_1day(city, params={}):
     response = generate_request('http://dataservice.accuweather.com/forecasts/v1/daily/1day/' 
-                                + city_id + '?apikey=' + key, params)
+                                + str(get_city_id(city)) + '?apikey=' + key, params)
     if response:
         weather_1day = response
-        print(weather_1day)
+        aux = json.dumps(weather_1day)
+        weather_1day_json =  json.loads(aux)
+        print(weather_1day['DailyForecasts'])
         return weather_1day
 
 if __name__ == "__main__":
-    get_city('Seville')
-    # get_weather_1day('306733')
+    get_weather_1day('Seville')
